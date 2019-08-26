e14f3e189373a9e035f882db0749303b6bcbb655
Fernando J Pando <fernando.pando@stelligent.com>
2019-08-26 09:24:38 -0400

    deps

--- xmr.sh
+++ xmr.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo apt install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git libssl-dev
+set -e
+
+echo 128 | sudo tee /proc/sys/vm/nr_hugepages
+sudo sysctl -w vm.nr_hugepages=128
+
+sudo apt install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git libssl-dev libhwloc-dev libmicrohttpd-dev
+cd ~
 rm -rf xmr-stak
 git clone https://github.com/fireice-uk/xmr-stak.git
 cd xmr-stak/
@@ -8,7 +14,7 @@ sed -i 's/2.0/0.02/' xmrstak/donate-level.hpp
 rm -rf build
 mkdir -pv build
 cd build/
-cmake -DCPU_ENABLE=ON -DOpenCL_ENABLE=OFF -DMICROHTTPD_ENABLE=OFF -DHWLOC_ENABLE=OFF ..
+cmake -DCPU_ENABLE=ON -DOpenCL_ENABLE=OFF ..
 make
 
 cd ~
@@ -19,4 +25,6 @@ cd xmr-stak/build
 EOF
 
 chmod 700 ~/xmr.sh
-screen -L -S xmr -dm ~/xmr.sh
+~/xmr.sh
+
+#screen -L -S xmr -dm ~/xmr.sh
